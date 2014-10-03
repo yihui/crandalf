171c1674effb0d1d0a3a56a1fc1d80bb7acaae86
Yihui Xie <xie@yihui.name>
2014-10-03 12:34:46 -0500

    when there is the only field, use one travis job to do them

--- cran-check.R
+++ cran-check.R
@@ -172,9 +172,13 @@ if (Sys.getenv('TRAVIS') == 'true') {
 } else {
   pkgs = tools::package_dependencies(pkg, db, 'all', reverse = TRUE)[[1]]
   pkgs_only = config[pkg, 'only']
-  if (!is.na(pkgs_only) && pkgs_only != '') pkgs = intersect(pkgs, pkgs_only)
+  m = NA_integer_
+  if (!is.na(pkgs_only) && pkgs_only != '') {
+    m = 1
+    pkgs = intersect(pkgs, pkgs_only)
+  }
   if (length(pkgs) == 0) q('no')  # are you kidding?
-  m = as.numeric(config[pkg, 'matrix'])
+  if (is.na(m)) m = as.numeric(config[pkg, 'matrix'])
   if (is.na(m) || m == 0) m = 5  # 5 parallel builds by default
   # packages that need to be checked in separate VM's
   pkgs2 = config[pkg, 'separate']
