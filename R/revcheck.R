ecfa6f3d4a38d4f48d9ceb0ba8c8c7b53fb3a864
Yihui Xie <xie@yihui.name>
2021-03-06 22:13:24 -0600

    signal an error instead of warning when any rev deps are broken

--- R/revcheck.R
+++ R/revcheck.R
@@ -25,7 +25,7 @@ if (file.exists('00check_diffs.html')) {
   system('curl -F "file=@00check_diffs.html" https://file.io')
   r = '[.]Rcheck2$'
   pkgs = gsub(r, '', list.files('.', r))
-  warning(
+  stop(
     'Some reverse dependencies may be broken by the dev version of PKG_NAME: ',
     paste(pkgs, collapse = ' ')
   )
