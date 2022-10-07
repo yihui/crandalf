d86ff604c083410628fa7c25b769f4660d872b43
Yihui Xie <xie@yihui.name>
2022-10-07 14:59:19 -0500

    print out the check details
    [ci skip]

--- R/cloud.R
+++ R/cloud.R
@@ -7,7 +7,8 @@ revdepcheck::cloud_check(r_version = getRversion())
 revdepcheck::cloud_status(update_interval = 60)
 
 if (length(res <- revdepcheck::cloud_broken())) {
-  revdepcheck::cloud_summary()
+  print(revdepcheck::cloud_summary())
   revdepcheck::cloud_report()
+  for (p in res) print(revdepcheck::cloud_details(revdep = p))
   stop('Package(s) broken: ', paste(res, collapse = ' '))
 }
