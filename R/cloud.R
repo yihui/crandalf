75a274e96ed469793de307a4f233f3b2146e371c
Yihui Xie <xie@yihui.name>
2022-10-18 13:05:59 -0500

    turn numeric_version to character
    [ci skip]

--- R/cloud.R
+++ R/cloud.R
@@ -3,7 +3,7 @@ if (!requireNamespace('remotes', quietly = TRUE)) install.packages('remotes')
 remotes::install_github(c('r-lib/revdepcheck', commandArgs(TRUE)), dependencies = TRUE)
 setwd('package')
 
-revdepcheck::cloud_check(r_version = getRversion())
+revdepcheck::cloud_check(r_version = format(getRversion()))
 revdepcheck::cloud_status(update_interval = 60)
 
 if (length(res <- revdepcheck::cloud_broken())) {
