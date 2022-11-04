f8f6b13135640dd2ffa0c7f77a1a01e623fa4186
Yihui Xie <xie@yihui.name>
2022-11-04 09:23:27 -0500

    the cloud may not support the very latest version of R, so use the highest supported version in that case
    [ci skip]

--- R/cloud.R
+++ R/cloud.R
@@ -3,7 +3,19 @@ if (!requireNamespace('remotes', quietly = TRUE)) install.packages('remotes')
 remotes::install_github(c('r-lib/revdepcheck', commandArgs(TRUE)), dependencies = TRUE)
 setwd('package')
 
-revdepcheck::cloud_check(r_version = format(getRversion()))
+# if the current R version doesn't work, use the highest supported version
+tryCatch(
+  revdepcheck::cloud_check(r_version = format(getRversion())),
+  error = function(e) {
+    r = ".*?\\[(('([0-9.]+)'(,\\s+)?)+)].*"
+    x = grep(r, e$message, value = TRUE)
+    x = gsub(r, '\\1', x)
+    v = unlist(strsplit(x, "('|,\\s+)"))
+    v = v[v != ''][1]
+    if (is.na(v)) stop(e)
+    revdepcheck::cloud_check(r_version = v)
+  }
+)
 revdepcheck::cloud_status(update_interval = 60)
 
 if (length(res <- revdepcheck::cloud_broken())) {
