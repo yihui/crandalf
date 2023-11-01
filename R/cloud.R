08e8ba8cdfd9f396ee5032c9239cc476ba85d601
Yihui Xie <xie@yihui.name>
2023-11-01 11:57:36 -0500

    install xfun from Github directly

--- R/cloud.R
+++ R/cloud.R
@@ -1,7 +1,6 @@
 update.packages(ask = FALSE, checkBuilt = TRUE)
 if (!requireNamespace('remotes', quietly = TRUE)) install.packages('remotes')
-remotes::install_github(c('r-lib/revdepcheck', commandArgs(TRUE)), dependencies = TRUE)
-install.packages('xfun', repos = 'https://yihui.r-universe.dev')
+remotes::install_github(c('r-lib/revdepcheck', 'yihui/xfun', commandArgs(TRUE)), dependencies = TRUE)
 if (dir.exists('package')) {
   setwd('package')
   xfun:::cloud_check()
