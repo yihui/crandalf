e8a04a4a8f29619974fd0e9bdbbfcb7bf253fa97
Yihui Xie <xie@yihui.name>
2023-11-01 21:31:22 -0500

    make it possible to check specific rev deps

--- R/cloud.R
+++ R/cloud.R
@@ -1,7 +1,8 @@
 update.packages(ask = FALSE, checkBuilt = TRUE)
 if (!requireNamespace('remotes', quietly = TRUE)) install.packages('remotes')
-remotes::install_github(c('r-lib/revdepcheck', 'yihui/xfun', commandArgs(TRUE)), dependencies = TRUE)
+a = commandArgs(TRUE)
+remotes::install_github(c('r-lib/revdepcheck', 'yihui/xfun', if (length(a)) a[1]), dependencies = TRUE)
 if (dir.exists('package')) {
   setwd('package')
-  xfun:::cloud_check()
+  xfun:::cloud_check(if (length(a) > 1) a[-1])
 }
