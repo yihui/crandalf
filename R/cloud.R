30a76d540837bb0240c5d51f856562169295fd0b
Yihui Xie <xie@yihui.name>
2023-11-01 21:42:08 -0500

    just remotes::install_local() since the package has been checked out (also with the desired ref instead of HEAD)

--- R/cloud.R
+++ R/cloud.R
@@ -1,8 +1,8 @@
 update.packages(ask = FALSE, checkBuilt = TRUE)
 if (!requireNamespace('remotes', quietly = TRUE)) install.packages('remotes')
-a = commandArgs(TRUE)
-remotes::install_github(c('r-lib/revdepcheck', 'yihui/xfun', if (length(a)) a[1]), dependencies = TRUE)
+remotes::install_github(c('r-lib/revdepcheck', 'yihui/xfun'))
 if (dir.exists('package')) {
   setwd('package')
-  xfun:::cloud_check(if (length(a) > 1) a[-1])
+  remotes::install_local(dependencies = TRUE)
+  xfun:::cloud_check(commandArgs(TRUE))
 }
