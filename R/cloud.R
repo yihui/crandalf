0a0e0834dda17007bf8f67d5d2078802147bf142
Yihui Xie <xie@yihui.name>
2024-08-16 09:17:34 -0500

    quiet install

--- R/cloud.R
+++ R/cloud.R
@@ -1,8 +1,8 @@
-update.packages(ask = FALSE, checkBuilt = TRUE)
-if (!requireNamespace('remotes', quietly = TRUE)) install.packages('remotes')
-remotes::install_github(c('r-lib/revdepcheck', 'yihui/xfun'))
+update.packages(ask = FALSE, checkBuilt = TRUE, quiet = TRUE)
+if (!requireNamespace('remotes', quietly = TRUE)) install.packages('remotes', quiet = TRUE)
+remotes::install_github(c('r-lib/revdepcheck', 'yihui/xfun'), quiet = TRUE)
 if (dir.exists('package')) {
   setwd('package')
-  remotes::install_local(dependencies = TRUE)
+  remotes::install_local(dependencies = TRUE, quiet = TRUE)
   xfun:::cloud_check(gsub('[, ]', '', commandArgs(TRUE)))
 }
